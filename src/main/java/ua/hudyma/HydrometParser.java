0fcdddd4fda02634cc91f976c4571450488b4967
yurkohudyma <hudyma@gmail.com>
2025-04-29 10:42:55 +0300

    some fixes and improvements

--- src/main/java/ua/hudyma/HydrometParser.java
+++ src/main/java/ua/hudyma/HydrometParser.java
@@ -10,16 +10,66 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+import static java.lang.System.*;
+
 public class HydrometParser {
+
+    static final String OUTPUT_DATA_FILENAME = "csv//" + getDate() + ".csv";
+    public static final String HYDROMET_URL = "http://gmc.uzhgorod.ua/metdata.php?StNo=33646";
+    static Map<String, List<String>> map;
+    static Map<String, List<HydrometUnit>> hydroMap;
+
     public static void main(String[] args) throws IOException {
         parseHydroUrl();
-        var map = convertCsvToMapList("csv//output.csv");
-        var hydroMap = convertMapListToModelMapList(map);
-        System.out.println(hydroMap);
+        convertCsvToMapList();
+        convertMapListToModelMapList();
+        out.println("Max т-ра: " + findMaxTemp() + " град");
+        out.println("Min т-ра: " + findMinTemp() + " град");
     }
 
-    private static Map<String, List<HydrometUnit>> convertMapListToModelMapList(Map<String, List<String>> map) {
-        var hydroMap = new HashMap<String, List<HydrometUnit>>();
+    private static void parseHydroUrl() throws IOException {
+        if (new File(OUTPUT_DATA_FILENAME).exists()) {
+            out.println("file exists, skipping parsing");
+            return;
+        }
+        Document document = Jsoup.connect(HYDROMET_URL).get();
+        Element table = document.select("table").first();
+        Elements rows = table.select("tr");
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(OUTPUT_DATA_FILENAME))) {
+            for (Element row : rows) {
+                Elements cols = row.select("td");
+                for (Element col : cols) {
+                    writer.print(col.text() + ",");
+                }
+                writer.println();
+            }
+        }
+    }
+
+    private static void convertCsvToMapList() throws FileNotFoundException {
+        map = new HashMap<>();
+        var scan = new Scanner(new FileReader(HydrometParser.OUTPUT_DATA_FILENAME));
+        scan.nextLine();
+        while (scan.hasNext()) {
+            var scanLine = scan.nextLine().split(" ");
+            String currentDate = scanLine[0];
+            var dataStr = scanLine[1];
+            dataStr = scanLine.length > 2 ? dataStr += scanLine[2] : dataStr;
+            List<String> datalist;
+            if (map.containsKey(currentDate)) {
+                datalist = map.get(currentDate);
+            } else {
+                datalist = new ArrayList<>();
+            }
+            datalist.add(dataStr);
+            map.put(currentDate, datalist);
+        }
+        scan.close();
+    }
+
+    private static void convertMapListToModelMapList() {
+        hydroMap = new HashMap<>();
         for (Map.Entry<String, List<String>> entry : map.entrySet()) {
             var key = entry.getKey();
             var rawList = entry.getValue();
@@ -33,13 +83,11 @@ public class HydrometParser {
                 } else {
                     unit.setTemp(array[1]);
                 }
-                if (array[2].startsWith(".")){
+                if (array[2].startsWith(".")) {
                     unit.setDewPoint("0" + array[2]);
-                }
-                else {
+                } else {
                     unit.setDewPoint(array[2]);
                 }
-                unit.setDewPoint(array[2]);
                 unit.setElements(array[3]);
                 unit.setWindDirection(array[5]);
                 unit.setWindSpeed(array[6]);
@@ -47,51 +95,35 @@ public class HydrometParser {
             }
             hydroMap.put(key, hydroList);
         }
-        return hydroMap;
     }
 
-    private static void parseHydroUrl() throws IOException {
-        String url = "http://gmc.uzhgorod.ua/metdata.php?StNo=33646";
-        Document document = Jsoup.connect(url).get();
-        Element table = document.select("table").first();
-        Elements rows = table.select("tr");
-        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        try (PrintWriter writer = new PrintWriter(new FileWriter("csv//" + date + ".csv"))) {
-            for (Element row : rows) {
-                Elements cols = row.select("td");
-                for (Element col : cols) {
-                    writer.print(col.text() + ",");
-                }
-                writer.println();
-            }
-        }
+    private static String getDate() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
     }
 
-    private static Map<String, List<String>> convertCsvToMapList(String filename) throws FileNotFoundException {
-        var map = new HashMap<String, List<String>>();
-        var scan = new Scanner(new FileReader(filename));
-        scan.nextLine();
-        while (scan.hasNext()) {
-            var scanLine = scan.nextLine().split(" ");
-            String currentDate = scanLine[0];
-            var dataStr = scanLine[1];
-            dataStr = scanLine.length > 2 ? dataStr += scanLine[2] : dataStr;
-            List<String> datalist;
-            if (map.containsKey(currentDate)) {
-                datalist = map.get(currentDate);
-                datalist.add(dataStr);
-                map.put(currentDate, datalist);
-            } else {
-                datalist = new ArrayList<>();
-                datalist.add(dataStr);
-                map.put(currentDate, datalist);
-            }
-        }
-        scan.close();
-        return map;
+    private static float findMinTemp() {
+        return hydroMap
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .map(e -> e.temp)
+                .map(Float::parseFloat)
+                .reduce(Math::min).orElseThrow();
+    }
+
+    private static float findMaxTemp() {
+        return hydroMap
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .map(e -> e.temp)
+                .map(Float::parseFloat)
+                .reduce(Math::max)
+                .orElseThrow();
     }
 
     static class HydrometUnit {
+
         String time, temp, dewPoint, elements, windDirection, windSpeed;
 
         public void setTime(String time) {
